4c4c51cd45a175a6d62245954fbb4e4f8c40bcc9
dipesh20500 <dipesh20500@gmail.com>
2016-03-26 01:13:32 +0530

    material.angularjs Implementation - personal detail overview

--- mockup/js/index.js
+++ mockup/js/index.js
@@ -1,20 +1,21 @@
 angular
   .module('MyApp',['ngMaterial'])
   .controller('DemoCtrl', function($scope) {
-      $scope = {
-	      first_name: 'John',
-	      last_name: 'M Doe',
-	      email: 'john_koshy@hotmail.com',
-	      address_line1: '23 Main St. Washington DC, DC-12345',
-	      dateofbirth: '04 Jan 1980',
-	      fax: 'xxx-xx-1111',
-	      m_daytime_phone: '(123) - 450 - 7890',
-	      city: 'Select a City',
-	      cityies: ['Select a City','city1','city2','city3'],
-	      state: 'Select a State',
-	      states: ['Select a State','state1','state2','state3']
-	      
-    };
+      $scope.first_name = 'John';
+	  $scope.last_name = 'M Doe';
+      $scope.email = 'john_koshy@hotmail.com';
+      $scope.address_line1 = '23 Main St. Washington DC, DC-12345';
+      $scope.dateofbirth = '04 Jan 1980';
+      $scope.fax = 'xxx-xx-1111';
+      $scope.m_daytime_phone = '(123) - 450 - 7890';
+      $scope.city = 'Select a City';
+      $scope.cityies = ['city1','city2','city3'];
+      $scope.state = 'Select a State';
+      $scope.states = ['state1','state2','state3'];
+      $scope.b_gender = '';
+      $scope.genders = ['Male', 'Female'];
+	  $scope.countrys = ['country1','country2','country3'];
+      
   });
 
 
