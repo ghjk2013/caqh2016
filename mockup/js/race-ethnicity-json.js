2c5e5655c8ff1bb709027c9c8ace3d4d9b3b9425
dipesh20500 <dipesh20500@gmail.com>
2016-05-01 21:24:41 +0530

    Ethnics JSON integration

---
+++ mockup/js/race-ethnicity-json.js
@@ -0,0 +1,12 @@
+function GlobalEthnicity() {
+	this.types = [
+{value:1, text:'American Indian or Alaska Native'},
+{value:2, text:'Asian'},
+{value:3, text:'Black of African American'},
+{value:4, text:'Hispanic or Latino'},
+{value:5, text:'Native Hawaiian or Other Pacific Islander'},
+{value:6, text:'White/Caucasian'},
+{value:7, text:'Other'},
+{value:8, text:'Prefer Not to Say'}
+];
+}
