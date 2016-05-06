0f7feb90fec92a4a15646924d22dba8fe6855fcc
dipesh20500 <dipesh20500@gmail.com>
2016-05-06 23:35:33 +0530

    Is there a way to suppress those parameters when I refresh the page - done for now, but the page is flickering we will look into it later. Specialty JSON integration Save change identification space All the controller to be created -- except practice location JSON data binding for all available JSON Practice location last tab not functional

---
+++ mockup/js/practice-type-json.js
@@ -0,0 +1,18 @@
+function GlobalPracticeType() {
+	this.types = [
+{value:6, text:'Group Primary Care'},
+{value:7, text:'Group/Single/Clinic'},
+{value:12, text:'Home Based'},
+{value:9, text:'Hospital Based'},
+{value:3, text:'Multi-Specialty Group'},
+{value:8, text:'Multi-Specialty Group/Other'},
+{value:11, text:'Other'},
+{value:10, text:'Partnership'},
+{value:2, text:'Single Specialty Group'},
+{value:1, text:'Solo Practice'},
+{value:4, text:'Solo Primary Care'},
+{value:5, text:'Solo Specialty Care'},
+{value:13, text:'Urgent Care'}
+];
+
+}
